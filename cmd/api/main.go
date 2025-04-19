809a9afd7db6b154f356be07cdb149a7edf48bd0
Roman <roma3io@mail.ru>
2025-04-19 17:28:42 +0700

    logging start

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,34 @@
 package main
 
-import "todo-app/internal/config"
+import (
+	"log/slog"
+	"os"
+	"todo-app/internal/config"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
 
 func main() {
 	cfg := config.MustLoad()
+	log := setupLogger(cfg.Env)
+	log = log.With(slog.String("env", cfg.Env))
+	log.Info("Initializing server", slog.String("address", cfg.Address))
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+	switch env {
+	case envLocal:
+		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envDev:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envProd:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	}
+
+	return log
 }
